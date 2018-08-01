bd361fba14a4a26fc0fa1483ffbe4175166adecb
Charles Zipp <charleszipp@gmail.com>
2018-08-01 06:39:22 -0500

    updated phase to use .netstandard

--- src/Phase/Domains/AggregateProxy.cs
+++ src/Phase/Domains/AggregateProxy.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.Remoting.Messaging;
-using System.Runtime.Remoting.Proxies;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,43 +8,33 @@ namespace Phase.Domains
     /// Responsible for controlling the number of threads that can execute against a single aggregate root instance at any given time.
     /// </summary>
     /// <typeparam name="TAggregate">Aggregate root type</typeparam>
-    public class AggregateProxy<TAggregate> : RealProxy
+    public class AggregateProxy<TAggregate> : DispatchProxy
         where TAggregate : AggregateRoot
     {
-        private readonly TAggregate _aggregate;
-        private readonly SemaphoreSlim _pool;
-
-        protected AggregateProxy(TAggregate aggregate)
-            : base(typeof(TAggregate))
-        {
-            _aggregate = aggregate;
-            _pool = new SemaphoreSlim(1, 1);
-        }
+        private TAggregate _aggregate;
+        private readonly SemaphoreSlim _pool = new SemaphoreSlim(1, 1);
 
         public static TAggregate Create(TAggregate aggregate)
         {
-            return (TAggregate)new AggregateProxy<TAggregate>(aggregate).GetTransparentProxy();
+            object proxy = Create<TAggregate, AggregateProxy<TAggregate>>();
+            ((AggregateProxy<TAggregate>)proxy)._aggregate = aggregate;
+            return (TAggregate)proxy;
         }
 
-        public override IMessage Invoke(IMessage msg)
+        protected override object Invoke(MethodInfo targetMethod, object[] args)
         {
             try
             {
                 _pool.Wait();
-                var methodCall = (IMethodCallMessage)msg;
-                var method = (MethodInfo)methodCall.MethodBase;
-                var result = method.Invoke(_aggregate, methodCall.InArgs);
+                var result = targetMethod.Invoke(_aggregate, args);
                 if (result is Task)
                     ((Task)result).Wait();
 
-                return new ReturnMessage(result, null, 0, methodCall.LogicalCallContext, methodCall);
+                return result;
             }
-            catch (Exception ex)
+            catch (Exception ex) when (ex is TargetInvocationException)
             {
-                if (ex is TargetInvocationException && ex.InnerException != null)
-                    return new ReturnMessage(ex.InnerException, msg as IMethodCallMessage);
-
-                return new ReturnMessage(ex, msg as IMethodCallMessage);
+                throw ex.InnerException ?? ex;
             }
             finally
             {
